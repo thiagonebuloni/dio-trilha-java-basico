df46a6b122b91e7095e19bb4d40de845e5cfe83e
Thiago Nebuloni <thiago.nebuloni@gmail.com>
2024-10-17 08:21:09 -0300

    entrega do projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        // Exibir as mensagens para o usuário
+        // Obter pela scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+        
+        System.out.println("Por favor, digite o número da Conta: ");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite seu saldo inicial: ");
+        Double saldo = scanner.nextDouble();
+
+
+        // Exibir a mensagem conta criada com sucesso
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
